8c18670d5e89f2bb1a681318f517344a2268562e
Aman <crystalaman02@gmail.com>
2022-04-09 14:52:49 +0530

    First Commit

---
+++ JavaInDepth/src/Unit01/P1_Task01_FirstProgramInJava.java
@@ -0,0 +1,11 @@
+package Unit01;
+
+public class P1_Task01_FirstProgramInJava {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World\n");
+		System.out.println("This is my first Java program\n");
+	}
+
+}
